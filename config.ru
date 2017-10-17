4dbfff9d20eaf8c8ae9a6de8c3f4843de6af6794
Joe Corcoran <joe@corcoran.io>
2017-10-17 12:10:47 +0200

    Extract auth to middleware

--- config.ru
+++ config.ru
@@ -1,2 +1,4 @@
 require 'travis/yaml/web'
+require 'travis/yaml/web/basic_auth'
+use Travis::Yaml::Web::BasicAuth
 run Travis::Yaml::Web
--- lib/travis/yaml/web/v1/routes/route.rb
+++ lib/travis/yaml/web/v1/routes/route.rb
@@ -1,31 +1,14 @@
-require 'travis/yaml/web/config'
-
 module Travis::Yaml::Web::V1
   module Routes
     module Route
       def call(env)
-        return not_authenticated unless authenticated?(env)
         public_send(env['REQUEST_METHOD'.freeze].downcase, env)
       end
 
       private
 
-      def config
-        @config ||= Travis::Yaml::Web::Config.load
-      end
-
       def method_missing(*)
         [404, {}, []]
-      end
-
-      def authenticated?(env)
-        auth = Rack::Auth::Basic::Request.new(env)
-        return false unless auth.provided? && auth.basic? && auth.credentials
-        config.auth_keys.any? { |key| Rack::Utils.secure_compare(key, auth.credentials.last) }
-      end
-
-      def not_authenticated
-        [401, { 'WWW-Authenticate'.freeze => 'Basic realm="Restricted area"'.freeze }, []]
       end
     end
   end
