46e5c35e5f8861d2e33f4e87096eac4580ddbbe3
Joe Corcoran <joe@corcoran.online>
2018-05-28 13:31:20 +0200

    Add new gh pages options

--- lib/travis/yaml/spec/def/deploy/pages.rb
+++ lib/travis/yaml/spec/def/deploy/pages.rb
@@ -9,15 +9,20 @@ module Travis
 
             def define
               super
-              map :github_token,  to: :str, secure: true
-              map :repo,          to: :str
-              map :target_branch, to: :str
-              map :local_dir,     to: :str
-              map :fqdn,          to: :str
-              map :project_name,  to: :str
-              map :email,         to: :str
-              map :name,          to: :str
-              map :github_url,    to: :str
+              map :github_token,       to: :str, secure: true
+              map :repo,               to: :str
+              map :target_branch,      to: :str
+              map :local_dir,          to: :str
+              map :fqdn,               to: :str
+              map :project_name,       to: :str
+              map :email,              to: :str
+              map :name,               to: :str
+              map :github_url,         to: :str
+              map :keep_history,       to: :bool
+              map :verbose,            to: :bool
+              map :allow_empty_commit, to: :bool
+              map :committer_from_gh,  to: :bool
+              map :deployment_file,    to: :bool
             end
           end
         end
