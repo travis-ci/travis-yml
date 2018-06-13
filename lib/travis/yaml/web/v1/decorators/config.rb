9701aa992d2acba49037ca046754634bd4aab396
Sven Fuchs <me@svenfuchs.com>
2018-06-13 17:05:18 +0200

    fix sorting if for some reason a message is missing a level

--- lib/travis/yaml/web/v1/decorators/config.rb
+++ lib/travis/yaml/web/v1/decorators/config.rb
@@ -3,7 +3,7 @@ require 'travis/yaml'
 module Travis::Yaml::Web::V1
   module Decorators
     class Config
-      LEVELS = %i(error warning info)
+      LEVELS = %i(error warning info other)
 
       def initialize(config)
         @config = config
@@ -37,7 +37,7 @@ module Travis::Yaml::Web::V1
       end
 
       def sort(msgs)
-        msgs.sort_by { |msg| LEVELS.index(msg.first) }
+        msgs.sort_by { |msg| LEVELS.index(msg.first || :other) }
       end
     end
   end
