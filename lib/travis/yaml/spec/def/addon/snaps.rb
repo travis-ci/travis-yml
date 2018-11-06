7a426331a173421bbc20182f88411e030a681236
Josh Kalderimis <josh.kalderimis@gmail.com>
2018-11-06 16:41:21 +0000

    first run at a snaps addon

---
+++ lib/travis/yaml/spec/def/addon/snaps.rb
@@ -0,0 +1,21 @@
+require 'travis/yaml/spec/type/map'
+require 'travis/yaml/spec/type/seq'
+
+module Travis
+  module Yaml
+    module Spec
+      module Def
+        module Addon
+          class Snaps < Type::Seq
+            register :snaps
+
+            def define
+              downcase
+              type :scalar, :seq, strict: false
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/travis/yaml/accept/addons/snaps_spec.rb
@@ -0,0 +1,24 @@
+describe Travis::Yaml, 'addon: snaps' do
+    let(:msgs)   { subject.msgs }
+    let(:addons) { subject.serialize[:addons] }
+  
+    subject { described_class.apply(config.merge(language: 'ruby')) }
+  
+    describe 'snaps' do
+      describe 'given a string' do
+        let(:config) { { addons: { snaps: 'travis' } } }
+        it { expect(addons[:hosts]).to eq ['travis'] }
+      end
+  
+      describe 'given an array' do
+        let(:config) { { addons: { snaps: ['travis', 'aws-cli'] } } }
+        it { expect(addons[:snaps]).to eq ['travis', 'aws-cli'] }
+      end
+
+      describe 'given an array of mixed case strings' do
+        let(:config) { { addons: { snaps: ['Travis', 'AWS-cli'] } } }
+        it { expect(addons[:snaps]).to eq ['travis', 'aws-cli'] }
+      end
+    end
+  end
+  
