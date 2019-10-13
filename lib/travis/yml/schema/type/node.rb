cd0688b073cffdaf61021872a05207a61322e296
Sven Fuchs <me@svenfuchs.com>
2019-10-13 16:18:09 +0200

    allow :to to be an array, and return an any with the given types

--- lib/travis/yml/schema/type/node.rb
+++ lib/travis/yml/schema/type/node.rb
@@ -20,6 +20,7 @@ module Travis
 
           class << self
             def build(type, attrs = {})
+              return build(:any, types: type) if type.is_a?(Array)
               caching(resolve(type), attrs) do |type|
                 node = type.new(self, only(attrs, :types))
                 node = Form.apply(node)
