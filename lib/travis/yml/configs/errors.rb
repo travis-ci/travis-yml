9bd7f34c5efb2496ca0699375b3a02ee20fa7fa0
Sven Fuchs <me@svenfuchs.com>
2020-02-25 21:35:40 +0100

    check for 403 status

--- lib/travis/yml/configs/errors.rb
+++ lib/travis/yml/configs/errors.rb
@@ -51,7 +51,7 @@ module Travis
 
         def api_error(*args)
           case args.last.status
-          when 401 then unauthorized(*args)
+          when 401, 403 then unauthorized(*args)
           when 404 then not_found(*args)
           when 500 then server_error(*args)
           else raise(args.last)
