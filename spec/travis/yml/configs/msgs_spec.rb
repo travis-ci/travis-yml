152e904c92eb3534ddd77090687731ab669e1570
Sven Fuchs <me@svenfuchs.com>
2020-03-17 00:05:51 +0100

    parametrize merging unnormalized or normalized configs

--- spec/travis/yml/configs/msgs_spec.rb
+++ spec/travis/yml/configs/msgs_spec.rb
@@ -6,35 +6,71 @@ describe Travis::Yml::Configs, 'msgs' do
 
   subject { described_class.new(repo, 'master', nil, nil, {}, opts).tap(&:load) }
 
-  describe 'defaults', defaults: true do
-    yaml ''
-    it { expect(subject.config).to eq defaults }
-    it { should have_msg [:info, :root, :default, key: 'language', default: 'ruby'] }
-    it { should have_msg [:info, :root, :default, key: 'os', default: 'linux'] }
-    it { should have_msg [:info, :root, :default, key: 'dist', default: 'xenial'] }
+  describe 'merge_normalized not given' do
+    describe 'defaults', defaults: true do
+      yaml ''
+      it { expect(subject.config).to eq defaults }
+      it { should have_msg [:info, :root, :default, key: 'language', default: 'ruby'] }
+      it { should have_msg [:info, :root, :default, key: 'os', default: 'linux'] }
+      it { should have_msg [:info, :root, :default, key: 'dist', default: 'xenial'] }
+    end
+
+    describe 'empty', empty: true do
+      yaml 'script:'
+      it { expect(subject.config).to eq script: [] }
+      it { should have_msg [:warn, :script, :empty, key: 'script'] }
+    end
+
+    describe 'unknown' do
+      yaml %(
+        unknown: str
+      )
+      it { expect(subject.config).to eq unknown: 'str' }
+      it { should have_msg [:warn, :root, :unknown_key, key: 'unknown', value: 'str'] }
+    end
+
+    describe 'alert', alert: true do
+      yaml %(
+        deploy:
+          - provider: pages
+            token: str
+      )
+      it { expect(subject.config).to eq deploy: [provider: 'pages', token: 'str'] }
+      it { should have_msg [:alert, :'deploy.token', :secure, type: :str] }
+    end
   end
 
-  describe 'empty', empty: true do
-    yaml 'script:'
-    it { expect(subject.config).to eq script: [] }
-    it { should have_msg [:warn, :script, :empty, key: 'script'] }
-  end
+  describe 'merge_normalized given', merge_normalized: true do
+    describe 'defaults', defaults: true do
+      yaml ''
+      it { expect(subject.config).to eq defaults }
+      it { should have_msg [:info, :root, :default, key: 'language', default: 'ruby'] }
+      it { should have_msg [:info, :root, :default, key: 'os', default: 'linux'] }
+      it { should have_msg [:info, :root, :default, key: 'dist', default: 'xenial'] }
+    end
 
-  describe 'unknown' do
-    yaml %(
-      unknown: str
-    )
-    it { expect(subject.config).to eq unknown: 'str' }
-    it { should have_msg [:warn, :root, :unknown_key, key: 'unknown', value: 'str'] }
-  end
+    describe 'empty', empty: true do
+      yaml 'script:'
+      it { expect(subject.config).to eq script: [] }
+      it { should have_msg [:warn, :script, :empty, key: 'script'] }
+    end
 
-  describe 'alert', alert: true do
-    yaml %(
-      deploy:
-        - provider: pages
-          token: str
-    )
-    it { expect(subject.config).to eq deploy: [provider: 'pages', token: 'str'] }
-    it { should have_msg [:alert, :'deploy.token', :secure, type: :str] }
+    describe 'unknown' do
+      yaml %(
+        unknown: str
+      )
+      it { expect(subject.config).to eq unknown: 'str' }
+      it { should have_msg [:warn, :root, :unknown_key, key: 'unknown', value: 'str'] }
+    end
+
+    describe 'alert', alert: true do
+      yaml %(
+        deploy:
+          - provider: pages
+            token: str
+      )
+      it { expect(subject.config).to eq deploy: [provider: 'pages', token: 'str'] }
+      it { should have_msg [:alert, :'deploy.token', :secure, type: :str] }
+    end
   end
 end
