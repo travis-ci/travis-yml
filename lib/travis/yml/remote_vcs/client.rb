e001951e6e2523a286360ab8066e0529a3404663
Artur Kremens <artur.kremens@gmail.com>
2020-09-08 10:53:30 +0200

    Indentation

--- lib/travis/yml/remote_vcs/client.rb
+++ lib/travis/yml/remote_vcs/client.rb
@@ -7,28 +7,28 @@ module Travis
       class Client
         private
 
-        def connection
-          @connection ||= Faraday.new(http_options.merge(url: config[:vcs][:url])) do |c|
-            c.request :authorization, :token, config[:vcs][:token]
-            c.request :retry, max: 5, interval: 0.1, backoff_factor: 2
-            c.request :json
-            c.use Faraday::Response::RaiseError
-            c.use Faraday::Adapter::NetHttp
+          def connection
+            @connection ||= Faraday.new(http_options.merge(url: config[:vcs][:url])) do |c|
+              c.request :authorization, :token, config[:vcs][:token]
+              c.request :retry, max: 5, interval: 0.1, backoff_factor: 2
+              c.request :json
+              c.use Faraday::Response::RaiseError
+              c.use Faraday::Adapter::NetHttp
+            end
           end
-        end
 
-        def http_options
-          { ssl: config[:ssl].to_h }
-        end
+          def http_options
+            { ssl: config[:ssl].to_h }
+          end
 
-        def request(method, name)
-          resp = connection.send(method) { |req| yield(req) }
-          JSON.parse(resp.body)
-        end
+          def request(method, name)
+            resp = connection.send(method) { |req| yield(req) }
+            JSON.parse(resp.body)
+          end
 
-        def config
-          Yml.config
-        end
+          def config
+            Yml.config
+          end
       end
     end
   end
