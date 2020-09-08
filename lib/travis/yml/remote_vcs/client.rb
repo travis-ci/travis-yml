ef843e49317a68a489257827ea1f74c85eecff96
Artur Kremens <artur.kremens@gmail.com>
2020-09-08 11:02:16 +0200

    test change

--- lib/travis/yml/remote_vcs/client.rb
+++ lib/travis/yml/remote_vcs/client.rb
@@ -8,6 +8,7 @@ module Travis
         private
 
         def connection
+          puts "Test 123"
           @connection ||= Faraday.new(http_options.merge(url: config[:vcs][:url])) do |c|
             c.request :authorization, :token, config[:vcs][:token]
             c.request :retry, max: 5, interval: 0.1, backoff_factor: 2
