fe54a6918db8c63f1edccc5f610f8c684b2dadb3
Sven Fuchs <me@svenfuchs.com>
2020-10-30 19:30:03 +0100

    silence logger during tests

--- lib/travis/yml/remote_vcs/repository.rb
+++ lib/travis/yml/remote_vcs/repository.rb
@@ -5,7 +5,7 @@ module Travis
     module RemoteVcs
       class Repository < Client
         def content(id:, path:, ref:)
-          logger.info("RemoteVcs Repository #{id}, #{path}")
+          logger.info("RemoteVcs Repository #{id}, #{path}") unless ENV['ENV'] == 'test'
           request(:get, __method__) do |req|
             req.url "repos/#{id}/contents/#{path}"
             req.params['ref'] = ref
