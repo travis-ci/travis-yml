a2c8398d766d72c21aa48d70123824647e61dbff
Vitalie D <vitalie.dev@gmail.com>
2022-07-01 12:04:19 +0300

    code reformat

--- spec/integrate/vault_spec.rb
+++ spec/integrate/vault_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'fileutils'
 require 'yaml'
 
@@ -10,9 +8,13 @@ describe Travis::Yml, integration_configs: true do
 
       it do
         config = described_class.load(yaml, alert: true, fix: true, defaults: true)
-        expect(config.msgs).to match_array([[:info, :root, :default, { default: 'ruby', key: 'language' }],
-                                            [:info, :root, :default, { default: 'linux', key: 'os' }],
-                                            [:info, :root, :default, { default: 'xenial', key: 'dist' }]])
+        expect(config.msgs).to match_array(
+          [
+            [:info, :root, :default, { default: 'ruby', key: 'language' }],
+            [:info, :root, :default, { default: 'linux', key: 'os' }],
+            [:info, :root, :default, { default: 'xenial', key: 'dist' }]
+          ]
+        )
       end
     end
 
@@ -21,23 +23,24 @@ describe Travis::Yml, integration_configs: true do
 
       it do
         config = described_class.load(yaml, alert: true, fix: true, defaults: true)
-        expect(config.msgs).to match_array([[:info, :root, :default, { default: 'ruby', key: 'language' }],
-                                            [:info, :root, :default, { default: 'linux', key: 'os' }],
-                                            [:info, :root, :default, { default: 'xenial', key: 'dist' }],
-                                            [:alert, :"vault.token", :secure, { type: :str }],
-                                            [:error,
-                                             :"vault.secrets.kv_api_ver",
-                                             :unknown_value,
-                                             { value: 'invalid_value' }],
-                                            [:error,
-                                             :"vault.secrets",
-                                             :invalid_type,
-                                             { actual: :map,
-                                               expected: :str,
-                                               value: { namespace: [{ random_key: 'ns1' },
-                                                                    'project_id/secret_key',
-                                                                    'project_id/secret_key2'] } }],
-                                            [:error, :vault, :required, { key: 'api_url' }]])
+        expect(config.msgs).to match_array(
+          [
+            [:info, :root, :default, { default: 'ruby', key: 'language' }],
+            [:info, :root, :default, { default: 'linux', key: 'os' }],
+            [:info, :root, :default, { default: 'xenial', key: 'dist' }],
+            [:alert, :"vault.token", :secure, { type: :str }],
+            [:error, :"vault.secrets.kv_api_ver", :unknown_value, { value: 'invalid_value' }],
+            [
+              :error, :"vault.secrets", :invalid_type,
+              {
+                actual: :map,
+                expected: :str,
+                value: { namespace: [{ random_key: 'ns1' }, 'project_id/secret_key', 'project_id/secret_key2'] }
+              }
+            ],
+            [:error, :vault, :required, { key: 'api_url' }]
+          ]
+        )
       end
     end
   end
@@ -48,9 +51,13 @@ describe Travis::Yml, integration_configs: true do
 
       it do
         config = described_class.load(yaml, alert: true, fix: true, defaults: true)
-        expect(config.msgs).to match_array([[:info, :root, :default, { default: 'ruby', key: 'language' }],
-                                            [:info, :root, :default, { default: 'linux', key: 'os' }],
-                                            [:info, :root, :default, { default: 'xenial', key: 'dist' }]])
+        expect(config.msgs).to match_array(
+          [
+            [:info, :root, :default, { default: 'ruby', key: 'language' }],
+            [:info, :root, :default, { default: 'linux', key: 'os' }],
+            [:info, :root, :default, { default: 'xenial', key: 'dist' }]
+          ]
+        )
       end
     end
 
@@ -59,18 +66,23 @@ describe Travis::Yml, integration_configs: true do
 
       it do
         config = described_class.load(yaml, alert: true, fix: true, defaults: true)
-        expect(config.msgs).to match_array([[:info, :root, :default, { key: 'language', default: 'ruby' }],
-                                            [:info, :root, :default, { key: 'os', default: 'linux' }],
-                                            [:info, :root, :default, { key: 'dist', default: 'xenial' }],
-                                            [:alert, :"jobs.include.vault.token", :secure, { type: :str }],
-                                            [:error, :"jobs.include.vault.secrets.kv_api_ver", :unknown_value,
-                                             { value: 'invalid_value' }],
-                                            [:error, :"jobs.include.vault.secrets", :invalid_type,
-                                             { expected: :str, actual: :map, value:
-                                               { namespace:
-                                                   [{ random_key: 'ns1' },
-                                                    'project_id/secret_key', 'project_id/secret_key2'] } }],
-                                            [:error, :"jobs.include.vault", :required, { key: 'api_url' }]])
+        expect(config.msgs).to match_array(
+          [
+            [:info, :root, :default, { key: 'language', default: 'ruby' }],
+            [:info, :root, :default, { key: 'os', default: 'linux' }],
+            [:info, :root, :default, { key: 'dist', default: 'xenial' }],
+            [:alert, :"jobs.include.vault.token", :secure, { type: :str }],
+            [:error, :"jobs.include.vault.secrets.kv_api_ver", :unknown_value, { value: 'invalid_value' }],
+            [
+              :error, :"jobs.include.vault.secrets", :invalid_type,
+              {
+                expected: :str, actual: :map,
+                value: { namespace: [{ random_key: 'ns1' }, 'project_id/secret_key', 'project_id/secret_key2'] }
+              }
+            ],
+            [:error, :"jobs.include.vault", :required, { key: 'api_url' }]
+          ]
+        )
       end
     end
   end
