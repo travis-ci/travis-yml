819443d04dfe3d2cf8b6288541c8ad1f5ef4d92e
piccadilly circus <baize.aspect0t@icloud.com>
2023-06-13 20:19:56 +0500

    Remove code that got added by mistake in cherry pick

--- lib/travis/yml/schema/def/job.rb
+++ lib/travis/yml/schema/def/job.rb
@@ -7,7 +7,6 @@ require 'travis/yml/schema/def/git'
 require 'travis/yml/schema/def/group'
 require 'travis/yml/schema/def/osx_image'
 require 'travis/yml/schema/def/services'
-require 'travis/yml/schema/def/keys'
 require 'travis/yml/schema/def/workspaces'
 require 'travis/yml/schema/type'
 
@@ -34,7 +33,6 @@ module Travis
             map :if,         to: :condition
             map :services
             map :group
-            map :keys
             map :workspaces
 
             map :before_install, to: :seq, summary: 'Scripts to run before the install stage'
