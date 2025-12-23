d6f2c513ba6d3e0e1419a36ddc0bd59b79be427d
nikolaTrichkovski <nikola.trichkovski@devtactics.net>
2025-12-23 14:08:50 +0100

    revert changes for strict;

--- lib/travis/yml/schema/def/notification/msteams.rb
+++ lib/travis/yml/schema/def/notification/msteams.rb
@@ -14,16 +14,8 @@ module Travis
 
               prefix :rooms
 
-              map :rooms,    to: :msteams_urls, summary: 'MS Teams webhook URLs'
+              map :rooms,    to: :seq, type: :secure, summary: 'MS Teams webhook URLs'
               map :on_pull_requests, to: :bool, summary: 'Whether to notify on pull requests'
-            end
-          end
-
-          class MsteamsUrls < Type::Seq
-            register :msteams_urls
-
-            def define
-              type :secure, strict: false
             end
           end
         end
